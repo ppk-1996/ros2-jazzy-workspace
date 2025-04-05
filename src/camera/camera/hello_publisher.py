298394fb852693f915876553808b7a7a508090ce
sudohex <ppk1996kyaw@gmail.com>
2025-04-05 02:23:09 +0000

    Setup camera package

--- src/camera/camera/hello_publisher.py
+++ src/camera/camera/hello_publisher.py
@@ -1,22 +1,25 @@
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
 
 class HelloPublisher(Node):
-    def __init__(self):
+    def __init__(self)->None:
         super().__init__('hello_publisher')
         self.publisher_ = self.create_publisher(String, 'hello_topic', 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
 
-    def timer_callback(self):
+    def timer_callback(self)->None:
         msg = String()
         msg.data = "Hello"
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing: {msg.data}')
 
-
-def main():
+    def get(self)->str:
+        s = "string"
+        return s
+def main()->None:
     rclpy.init()
     node = HelloPublisher()
     rclpy.spin(node)
